f93c3f2a0437b8d0340ccf339875ae1cf14ce6eb
Tung Nguyen <xuantung.1998@gmail.com>
2023-03-11 12:40:57 +0700

    Update crlf

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -2,6 +2,7 @@ import { DynamicModule, Module } from '@nestjs/common';
 import * as TelegramBot from 'node-telegram-bot-api';
 
 const botToken = `5992886322:AAHB0PYleaaxs8Lbpzns4A0Dtsp_ItZDLa4`;
+
 @Module({})
 export class TelegramModule {
   static register(): DynamicModule {

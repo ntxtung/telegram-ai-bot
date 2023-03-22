9e290ceed4588b6a0b1d9951d60831262e4c67df
Felix Nguyen <felix.nguyen@contemi.com>
2023-03-22 17:45:04 +0700

    Add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegramModule } from './telegram/telegram.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TelegramModule.register()],
+  imports: [TelegramModule.register(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,16 +1,52 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import * as TelegramBot from 'node-telegram-bot-api';
+import {
+  ChatCompletionRequestMessageRoleEnum,
+  Configuration,
+  OpenAIApi,
+} from 'openai';
+import { ChatCompletionRequestMessage } from 'openai/api';
 
-const botToken = `5992886322:AAHB0PYleaaxs8Lbpzns4A0Dtsp_ItZDLa4`;
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+const openAiApiKey = process.env.OPEN_AI_API_KEY;
+
+const sampleChats: ChatCompletionRequestMessage[] = [
+  {
+    role: ChatCompletionRequestMessageRoleEnum.System,
+    content: 'You are a very experienced software tester.',
+  },
+];
 @Module({})
 export class TelegramModule {
   static register(): DynamicModule {
     const bot = new TelegramBot(botToken, { polling: true });
-    bot.on('message', (msg) => {
+
+    const openAiConfig = new Configuration({
+      apiKey: openAiApiKey,
+    });
+    const openai = new OpenAIApi(openAiConfig);
+
+    bot.on('message', async (msg) => {
       const chatId = msg.chat.id;
 
+      sampleChats.push({
+        role: ChatCompletionRequestMessageRoleEnum.User,
+        content: msg.text,
+      });
+
+      const completion = await openai.createChatCompletion({
+        messages: sampleChats,
+        max_tokens: 2048,
+        temperature: 1,
+        top_p: 1,
+        model: 'gpt-3.5-turbo',
+      });
+      sampleChats.push({
+        role: completion.data.choices[0].message.role,
+        content: completion.data.choices[0].message.content,
+      });
       // send a message to the chat acknowledging receipt of their message
-      bot.sendMessage(chatId, 'Received your message');
+      await bot.sendMessage(chatId, completion.data.choices[0].message.content);
     });
     return {
       module: TelegramModule,
